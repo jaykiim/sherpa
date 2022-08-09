e52448e3499a29af26b8a9f7f700689e66c61c57
Jay <tidzmtm12@gmail.com>
2022-08-09 11:37:45 +0900

    .

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,18 +2,15 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-  secret: process.env.NEXT_PUBLIC_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
     maxAge: 30 * 24 * 60 * 60,
   },
   jwt: {
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   },
   debug: process.env.NODE_ENV === "development",
-  pages: {
-    signIn: "/login",
-  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
--- pages/login.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Login } from "../components";
-
-const LoginPage = () => {
-  return <Login />;
-};
-
-export default LoginPage;
